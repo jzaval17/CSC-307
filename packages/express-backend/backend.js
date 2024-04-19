1994c2f50e3bdb692f3493cd6b8d8cddab8dd779
jzaval17 <jzaval17@calpoly.edu>
2024-04-18 20:57:35 -0700

    Get request users

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -3,6 +3,40 @@ import express from "express";
 
 const app = express(); //instance of express
 const port = 8000;
+const users = {
+  users_list: [
+    {
+      id: "xyz789",
+      name: "Charlie",
+      job: "Janitor"
+    },
+    {
+      id: "abc123",
+      name: "Mac",
+      job: "Bouncer"
+    },
+    {
+      id: "ppp222",
+      name: "Mac",
+      job: "Professor"
+    },
+    {
+      id: "yat999",
+      name: "Dee",
+      job: "Aspring actress"
+    },
+    {
+      id: "zap555",
+      name: "Dennis",
+      job: "Bartender"
+    }
+  ]
+};
+
+app.get("/users", (req, res) => {
+  res.send(users);
+});
+
 
 app.use(express.json());
 
